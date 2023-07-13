3e5f9dfe6725fc6a3fc0e6e119237dcaab2fb392
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-07-13 18:52:08 +0200

    Config terminée

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("requette recue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "votre requette à bien été reçue !" });
+  next();
+});
+
 app.use((req, res) => {
-	res.json({message : "votre requette à bien été reçue !"})
+	console.log('Réponse envoyé avec succès !');
 })
 
 module.exports = app;
-
