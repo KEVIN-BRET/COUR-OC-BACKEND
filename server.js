ddc2edc4524068c26b9f20a9bd7dcbcb8317314a
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-07-13 17:30:06 +0200

    Installation de express

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const http = require("http");
 
-const server = http
+const server = http.createServer((req, res) => {
+  res.end("voila la reponse du serveur mise à jour automatiquement grace à nodemon !");
+});
+
+server.listen(process.env.PORT || 3000);
+
